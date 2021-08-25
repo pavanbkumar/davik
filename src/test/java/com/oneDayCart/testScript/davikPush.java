bd351bd986bf3d16679ded0961c7c8dee4024950
pavanbkumar <pavankumarb16196@gmail.com>
2021-08-25 16:07:32 +0530

    second commit

---
+++ src/test/java/com/oneDayCart/testScript/davikPush.java
@@ -0,0 +1,14 @@
+package com.oneDayCart.testScript;
+
+import org.testng.annotations.Test;
+
+public class davikPush {
+	
+	
+	
+	@Test
+	public void newMethod(){
+    System.out.println("Hello");
+	}
+
+}
